ddf55c73b48f852b2d22a1a5af2712cdce3b30f8
ivarsrb <ivars.rb@gmail.com>
2020-06-01 09:09:00 +0300

    Change api path to one without slash

--- server/router.go
+++ server/router.go
@@ -13,9 +13,9 @@ func createRouter() *mux.Router {
 	router := mux.NewRouter()
 	// REST api serving
 	apirouter := router.PathPrefix("/api/notes").Subrouter()
-	apirouter.HandleFunc("/", getNotes).Methods(http.MethodGet)
+	apirouter.HandleFunc("", getNotes).Methods(http.MethodGet)
 	apirouter.HandleFunc("/{id:[0-9]+}", getNote).Methods(http.MethodGet)
-	apirouter.HandleFunc("/", postNote).Methods(http.MethodPost)
+	apirouter.HandleFunc("", postNote).Methods(http.MethodPost)
 	apirouter.HandleFunc("/{id:[0-9]+}", deleteNote).Methods(http.MethodDelete)
 	// SPA serving
 	spa := spaHandler{staticPath: "web", indexPath: "index.html"}
