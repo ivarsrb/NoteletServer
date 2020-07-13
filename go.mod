9aaf48ff7e4f26b073021ec005652fa095471e4b
ivarsrb <ivars.rb@gmail.com>
2020-07-13 11:04:19 +0300

    Add ping testing function

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/heroku/x v0.0.24
 	github.com/lib/pq v1.7.0
+	github.com/stretchr/testify v1.5.1
 )
--- server/handlers_test.go
+++ server/handlers_test.go
@@ -1,1 +1,18 @@
 package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPing(t *testing.T) {
+	router := newRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "OK", w.Body.String())
+}
